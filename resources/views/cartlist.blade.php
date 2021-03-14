470890a1ec1c8bf29b2c01fe59d84c5cc32cc74b
Zambezz <a.doctis@gmail.com>
2021-03-14 13:48:50 +0300

    Order detail Page
    Order detail Page

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -4,11 +4,13 @@
     <div class="col-sm-10">
         <div class="trending-wrapper">
             <h4>Result for Products</h4>
+            <a class="btn btn-success" href="/ordernow">Order Now</a> 
+            <br></br>
             @foreach($products as $item)
             <div class="row searched-item cart-list-divider">
                 <div class="col-sm-3">
                     <a href="/detail/{{$item->id}}">
-                    <img class="trending-image" src="{{$item->gallery}}">
+                        <img class="trending-image" src="{{$item->gallery}}">
                     </a>
                 </div>
                 <div class="col-sm-4">
@@ -18,11 +20,13 @@
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <button class="btn btn-warning"> Remove to cart</button>
+                    <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning"> Remove to cart</a>
                 </div>
             </div>
             @endforeach
         </div>
+        <a class="btn btn-success" href="/ordernow">Order Now</a> 
+         <br></br>
     </div>
 </div>
 @endsection
